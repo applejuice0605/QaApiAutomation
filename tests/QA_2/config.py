715082ac9fd65a6f3caad8b1a784e893d0ff905f
caishubin <Nora@fuse.co.id>
2026-02-27 15:35:11 +0800

    CS-7624 B2C

--- tests/QA_2/config.py
+++ tests/QA_2/config.py
@@ -8,13 +8,17 @@ sit_sf_chatflow_id = "f71f507b-da44-4d92-828b-6a37bf4d96a7"
 sit_domain = "https://pchat-sit.fuse.co.id/api"
 sit_chatflow_cekpremi = "52af7aad-e546-4ab1-a937-84a991de6117"
 sit_sf_v2 = '0a8391b8-33aa-4705-9b35-c2f2b54ad6a6'
+monica_b2c_token = 'http://172.16.198.197/v1'
+monica_b2c_chatFlow_app_id = '4305c12e-60c4-4fca-bca2-ae81043854aa'
+
+
 sit_user_info = {
-    "chat_temp_token": "eyJhbGciOiJIUzI1NiIsInppcCI6IkRFRiJ9.eNpEjMsOgjAQRf-la0g6dGiLP2CMGjHCgmXBMeKjNDyi0fjvjmy8y3PvPW8xTLVYiObsxoLuoeiu5EUkgutHT_0ukF8dud_kapmv91tbVVW65AEfvKdbOVA_D6yGzCqJCKiB-7p9Hbqpb4i7B8sHF8LfW7b8ASml1omcozITCXoGxkZLYxQig9aNP5BasDCDy9iyUFlS6HQaO9m4GHUGsVVwihNwqJWkpMZMfL4AAAD__w.QIDUvmMEV5JsaTWQjxcAA1HfZJ-owf7S2vmFycNNW_o",
-    "partner_uid": "1000662000000390",
+    "chat_temp_token": "eyJhbGciOiJIUzI1NiIsInppcCI6IkRFRiJ9.eNpEjMsOgjAQRf-la0haZmyLP2CMGjHCgiWUIeKjNjyi0fjvjmy8y3PvPW8xTLVYCneqxpxuIb9fyItIhKofPfX7QH7dcL_NYJVtDjtbluVixQM-eE_XYqB-HlitUgsSUaFW3Nfd63ifekfcPVg-VCH8vUXHHyWl1DqRcyA1kaBnYGy0QiMRMBJdNf7AwlpjZ3AeOxZqUKptGxO71rgYVQJxDdLFDlwtASlpEcTnCwAA__8.aJjJ3HXF7A9wufaBvPt1TzK7skoQ_iK-FdWMY3eKbXE",
+    "partner_uid": "1000662000000397",
     "tenant_id": "1000662",
-    "channel_user_id": "8613434915136",
+    "channel_user_id": "8619830441461",
     "channel_type": "whatsapp",
-    "session_id": "SN00OZA8D0I000000004R",
+    "session_id": "SN00PBZD689000000005D",
     "lan": "ID",
     "query": "how are you",
 }
